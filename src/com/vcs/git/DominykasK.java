f599a54e3488d56d39f1c303cf1582d7f5136137
Tidoo18 <dominykaskaleda@gmail.com>
2019-07-10 20:07:56 +0300

    Code Modifyed

--- src/com/vcs/git/DominykasK.java
+++ src/com/vcs/git/DominykasK.java
@@ -2,4 +2,13 @@ package com.vcs.git;
 
 public class DominykasK {
 
+	public String gitDone(String gitHub) {
+
+		if (gitHub.equals("feature/DominykasK")) {
+			System.out.println(gitHub + "true");
+		}
+		return gitHub + "false";
+
+	}
+
 }
